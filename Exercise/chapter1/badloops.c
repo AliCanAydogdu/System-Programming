90552597aee7c58be5aa6e05207af6dfc78524f7
Ali Can Aydogdu <alicantx@gmail.com>
2017-03-03 21:08:40 -0600

    Update badloops.c

--- Exercise/chapter1/badloops.c
+++ Exercise/chapter1/badloops.c
@@ -1,13 +1,3 @@
-
-	/* This program is supposed to find sequences of repeating letters.
-	** On sequences of 2 letters it works fine; on 3 or more letters it
-	** prints redundant lines of output.
-	**
-	** The debugger can be used to step through a loop, one line
-	** at a time, to observe what the code is doing. (pg 29) */
-
-// Fixed by Enes Kemal Ergin
-
 #include <stdio.h>
 
 int main() // int  added
